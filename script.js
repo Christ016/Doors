491b85729e7aa580c0feddc4fb5293497ddcf92b
Christ016 <125396489+Christ016@users.noreply.github.com>
2023-02-14 12:21:54 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+//* Declare variables below to save the different sections (divs) of your st
+let button1=document.querySelector(".option-one");
+let button2=document.querySelector(".option-two");
+let button3=document.querySelector(".option-three");
+let badending=document.querySelector(".option-one-screen");
+let goodending=document.querySelector(".option-two-screen");
+let neutralending=document.querySelector(".option-three-screen");
+
+
+button1.onclick=function(){
+  badending.style.display="block";  
+};
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
